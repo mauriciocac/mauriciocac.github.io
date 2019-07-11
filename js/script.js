5d2ebac9b11d8c4db19f87e70a5dab4352302d5c
mauriciocac <mauricio.cac@hotmail.com>
2019-07-11 17:21:32 -0300

    Alerta de Construcao do WebSite - Informacoes ficticias

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
+    alert('Esta página encontra-se em desenvolvimento. Algumas informações são fictícias.')
     $('.img-toggle, .ul-item li a').on('click', function () {
         $('.ul-item').toggleClass('isToggled');
     });
      
-
 });
